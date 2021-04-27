892ddaa3ad6c1971241ea10b85b7d3ee20889277
Remigiusz Drobinski <30534425+curiosis@users.noreply.github.com>
2021-04-27 22:09:17 +0200

    Add gameManager script

--- PingPong/Assets/GameManager.cs
+++ PingPong/Assets/GameManager.cs
@@ -2,12 +2,39 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using System;
 
 public class GameManager : MonoBehaviour
 {
-    public GameObject ball, player1, player1Goal, Player1Text;
-    public GameObject player2, player2Goal, Player2Text;
+    public GameObject ball, player1, player1Goal, Player1Text, Player1Effect;
+    public GameObject player2, player2Goal, Player2Text, Player2Effect;
     int Player1Score, Player2Score;
+
+    public GameObject gameInfo;
+
+    public GameObject addWallT, addWallB;
+    System.Random rnd = new System.Random();
+
+    public GameObject infoPanel;
+
+    private void Start()
+    {
+        updateGameInfo();
+        Player1Effect.GetComponent<TextMeshProUGUI>().text = "no effect";
+        Player2Effect.GetComponent<TextMeshProUGUI>().text = "no effect";
+    }
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.P))
+            infoPanel.SetActive(true);
+        else
+            infoPanel.SetActive(false);
+        if(Player1Score == 5 || Player2Score == 5)
+        {
+            Debug.Log("THE END!");
+        }
+    }
 
     public void Player1Scored()
     {
@@ -25,8 +52,40 @@ public class GameManager : MonoBehaviour
 
     void ResetPosition()
     {
+        player1.GetComponent<Movement>().Variety(rnd.Next(1, 8), 1);
+        player2.GetComponent<Movement>().Variety(rnd.Next(1, 8), 2);
+        player1.GetComponent<Movement>().updateInfo();
+        player2.GetComponent<Movement>().updateInfo();
         ball.GetComponent<Ball>().Reset();
         player1.GetComponent<Movement>().Reset();
         player2.GetComponent<Movement>().Reset();
+    }
+
+    public void Add_wall()
+    {
+        if (addWallT.activeSelf)
+        {
+            addWallT.SetActive(false);
+            addWallB.SetActive(false);
+        }
+        else
+        {
+            addWallT.SetActive(true);
+            addWallB.SetActive(true);
+        }
+    }
+
+    public void EffectText(String effectText, int playerNo)
+    {
+        if(playerNo == 1)
+            Player1Effect.GetComponent<TextMeshProUGUI>().text = effectText;
+        else
+            Player2Effect.GetComponent<TextMeshProUGUI>().text = effectText;
+        updateGameInfo();
+    }
+
+    void updateGameInfo()
+    {
+        gameInfo.GetComponent<TextMeshProUGUI>().text = "ball speed: " + ball.GetComponent<Ball>().speed + "\nadd wall: " + addWallT.activeSelf;
     }
 }
